b6f473d2c11ebb56bf168457d349814d5629fb1e
Usman <usmanabubakarsediq1@gmail.com>
2022-10-17 15:50:26 -0700

    add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include stdio.h
+
+/**
+ * main - Entry Point
+ * numbers between  0 and 9 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
