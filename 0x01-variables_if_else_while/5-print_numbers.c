4424cdda16313e5a0fa71ce13637870870eada6a
Usman <usmanabubakarsediq1@gmail.com>
2022-10-17 14:40:24 -0700

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * numbers between 0 and 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	if n(0 - 9)
+		printf("%d all single digit numbers of base 10 starting from 0\n", n)
+	return (0);
