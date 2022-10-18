2061db531fccb8ff56e53de57b74c710970aca70
Usman <usmanabubakarsediq1@gmail.com>
2022-10-18 09:06:05 -0700

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 'a'; m = 'z'; m++)
+	{
+		_putchar(m)
+	}
+	_putchar('\n')
+	return (0);
+}
