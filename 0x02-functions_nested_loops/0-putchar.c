d501463a3c1281651284718f65993c60359bd99c
Usman <usmanabubakarsediq1@gmail.com>
2022-10-18 07:01:16 -0700

    add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints putchar
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = putchar;)
+	{
+		putchar(a)
+	}
+	putchar('\n')
+	return (0);
+}
