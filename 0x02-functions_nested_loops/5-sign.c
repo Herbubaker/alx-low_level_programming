4f9ae835a82f7a9c8c4acd7c301a912ac970ff91
Usman <usmanabubakarsediq1@gmail.com>
2022-10-19 22:09:11 -0700

    add

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,26 +7,26 @@
 
 int print_sign(int n)
 {
-	 int low;
-	 int sign;
-	 
-	 if (n >= 1)
-	 {
-		 low = 1;
-		 _putchar('+');
-		 _putchar('\n');
-	 }
-	 else if (n < 0)
-	 {
-		 low = -1;
-		 _putchar('-');
-		 _putchar('\n');
-	 }
-	 else
-	 {
+	int low;
+	int sign;
+
+	if (n >= 1)
+	{
+		low = 1;
+		_putchar('+');
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		low = -1;
+		_putchar('-');
+		_putchar('\n');
+	}
+	else
+	{
 		low = 0;
 		_putchar('0');
 		_putchar('\n');
-	 }
-	 return (low)
+	}
+	return (low)
 }
